57a4275c5788352ca63facc6fbbf034fb49d385d
dimitricbrown <dimitricbrown@outlook.com>
2022-11-21 06:08:36 -0600

    Add all final code

---
+++ main.js
@@ -0,0 +1,10 @@
+// Code not needed anymore, the strings were added to the empty "fruits" array
+// const yellowFruit = "Banana"
+// const orangeFruit = "Orange"
+// const redFruit = "Apple"
+// const greenFruit = "Watermelon"
+// const blueFruit = "Blueberry"
+
+const fruits = ["Banana","Orange","Apple","Watermelon","Blueberry"]
+
+console.log(fruits)
